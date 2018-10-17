8641f007f6cca3f264f0cf6651033a6ff37c6a89
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-17 18:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ var katzDeliLine = [];
 var name = ['Ada', 'Grace', 'Kent']
 
 function takeANumber(line, name) {
-  return "Welcome, " + name + "you are number " + line + " in line."
+  return "Welcome, " + name + ". You are number " + line + " in line."
 }
