95d2d8ec6262a350ad1e2d78fc1e3b6a0ed948cb
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-17 18:51:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function currentLine(katzDeliLine) {
   var currently = []
 
   for (var i = 0; i < katzDeliLine.length; i++) {
-    let current = currently.push(i+1 + ". "+ katzDeliLine[i])
+    let current = currently.push(i+1 + ". " + katzDeliLine[i])
   }
   console.log(currently)
   return "The line is currently: " + currently
